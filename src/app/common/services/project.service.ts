17ed0a27dfefb70e4d81d3f816a66c9c24ef1fd2
Zeuxis Lo <seekstudio@gmail.com>
2018-06-16 13:30:17 +0800

    reverse the getProjects results

--- src/app/common/services/project.service.ts
+++ src/app/common/services/project.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from "@angular/core";
+
 import { Observable } from "rxjs/Observable";
+import { map } from "rxjs/operators";
+
 import { BaseService } from './base.service';
 
 @Injectable()
 export class ProjectService extends BaseService {
 
     public getProjects(): Observable<any> {
-        return this.client.get(this.apiUrl.projects)
+        return this.client.get(this.apiUrl.projects).pipe(
+            map(project => {
+                return project.sort((a, b) => {
+                    return (+b.id) - (+a.id)
+                });
+            })
+        );
     }
 
 }
