2c53a6ed05c46d14a376a809592f41e07c17f752
Zeuxis Lo <seekstudio@gmail.com>
2018-06-16 13:44:39 +0800

    fix the typescript error TS2339 about sort does not exist on type Object

--- src/app/common/services/project.service.ts
+++ src/app/common/services/project.service.ts
@@ -4,14 +4,15 @@ import { Observable } from "rxjs/Observable";
 import { map } from "rxjs/operators";
 
 import { BaseService } from './base.service';
+import { Project } from '../models/project.model';
 
 @Injectable()
 export class ProjectService extends BaseService {
 
     public getProjects(): Observable<any> {
         return this.client.get(this.apiUrl.projects).pipe(
-            map(project => {
-                return project.sort((a, b) => {
+            map(projects => {
+                return (projects as Project[]).sort((a, b) => {
                     return (+b.id) - (+a.id)
                 });
             })
